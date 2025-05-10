3fc5e3796dbc3ac988e6d686c760647ef9ce6086
Nikhil <kumarnikhil48578@gmail.com>
2025-05-10 18:14:53 +0530

    fix(ui): update termial  and Hero section content

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,4 @@
 // src/lib/utils.ts
-export function cn(...classes: string[]) {
-  return classes.filter(Boolean).join(' ');
+export function cn(...classes: (string | undefined | false | null)[]) {
+  return classes.filter(Boolean).join(" ");
 }
