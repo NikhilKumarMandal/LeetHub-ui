ef0a6a10f4fe35c9b3da03575b41e00277de6eb3
Nikhil <kumarnikhil48578@gmail.com>
2025-06-08 11:17:53 +0530

    update

--- src/Layouts/NonAuth.tsx
+++ src/Layouts/NonAuth.tsx
@@ -4,8 +4,7 @@ import { Navigate, Outlet } from "react-router-dom";
 function NonAuth() {
   const { user } = useAuthStore();
   if (user !== null) {
-    const returnTo =
-    new URLSearchParams(window.location.search).get("returnTo") || "/auth/home";
+    const returnTo = new URLSearchParams(location.search).get('returnTo') || "https://www.leethub.tech/auth/home";
     return <Navigate to={returnTo} replace={true} />;
   }
   return (
