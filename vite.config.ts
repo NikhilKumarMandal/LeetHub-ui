c700876aaf1ab7b76ec3432f88c308127e661edb
Nikhil <kumarnikhil48578@gmail.com>
2025-06-08 11:11:26 +0530

    fix: issue

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
-    },
-  },
-  build: {
-    outDir: "dist",
-  },
-  server: {
-    fs: {
-      allow: ["."],
-    },
-  },
-  preview: {
-    headers: {
-      "Cache-Control": "no-cache",
     },
   },
 });
