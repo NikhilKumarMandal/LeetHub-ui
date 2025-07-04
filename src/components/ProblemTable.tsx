91e81d9e6215c66a25859d088c90a5b7aff96e4e
Nikhil <kumarnikhil48578@gmail.com>
2025-07-04 15:13:17 +0530

    fix: bugs

--- src/components/ProblemTable.tsx
+++ src/components/ProblemTable.tsx
@@ -92,6 +92,9 @@ export function ProblemsTable() {
     placeholderData: keepPreviousData,
   });
 
+  console.log(data);
+  
+
   const { data: playlist } = useQuery({
     queryKey: ["playlist"],
     queryFn: () => {
